3eb60c5139258cdc7c86abdea6ccf4dea909ffec
jhsieh731 <jhsieh@college.harvard.edu>
2024-05-06 19:01:56 -0400

    updates

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def home():
     return render_template('index.html')
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
